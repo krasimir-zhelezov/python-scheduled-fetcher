a2db3e80b01849ed736c6b847c37aa9cf6dde584
krasimir-zhelezov <krasizh07@gmail.com>
2025-05-16 17:55:32 +0300

    feat: added logging

--- main.py
+++ main.py
@@ -4,17 +4,26 @@ import requests
 import datetime
 import os
 from dotenv import load_dotenv
+import logging
 
 load_dotenv()
 
 app = Flask(__name__)
 
+logging.basicConfig(
+    filename='.log',
+    filemode='a', 
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+
 def make_external_request():
     try:
         response = requests.get(os.environ.get('URL'))
-        print(f"[{datetime.datetime.now()}] Request made to external site. Status code: {response.status_code}")
+        logging.info(f'Request made to external site. Status code: {response.status_code}')
     except Exception as e:
-        print(f"Error making request: {e}")
+        logging.error(f'Error while making request: {e}')
 
 scheduler = BackgroundScheduler()
 scheduler.add_job(func=make_external_request, trigger="interval", minutes=int(os.environ.get('INTERVAL_IN_MINUTES')))
